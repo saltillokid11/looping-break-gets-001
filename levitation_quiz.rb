6acab7b0081411d6a61e9289c58eed866c0c9cae
Tanner Lancaster <tanner@flatironschool.com>
2018-05-29 21:17:44 +0000

    Done.

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,6 +1,10 @@
 
 def levitation_quiz
-	#your code here
+	puts "What is the spell that enacts levitation?"
+  answer = gets.chomp
+  if answer == "Wingardium Leviosa"
+    puts "You passed the quiz!"
+  else
+    levitation_quiz
+  end
 end
-
-
